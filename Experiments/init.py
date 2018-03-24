b6060bb6314f10a497adfe314352017051884ee6
Wolfgang Pfaff <wolfgangpfff@gmail.com>
2018-03-24 15:32:46 +0100

    latest work

--- Experiments/init.py
+++ Experiments/init.py
@@ -82,36 +82,36 @@ for i in range(1,5):
 
 ### Creating instruments
 def init_instruments():
-    from stationq.qctools import instruments as instools
+    from pytopo.qctools import instruments as instools
 
     from qcodes.instrument_drivers.QuTech.IVVI import IVVI
     ivvi = instools.create_inst(IVVI, "ivvi", "ASRL5::INSTR")
 
-    #from qcodes.instrument_drivers.stanford_research.SR865 import SR865
-    #sr1 = instools.create_inst(SR865, "sr1", "GPIB0::3::INSTR")
+    # from qcodes.instrument_drivers.stanford_research.SR865 import SR865
+    # sr1 = instools.create_inst(SR865, "sr1", "USB0::0xB506::0x2000::003104::INSTR")
     #
     from qcodes.instrument_drivers.Keysight.Keysight_34465A import Keysight_34465A
-    key1 = instools.create_inst(Keysight_34465A, "key1", "USB0::0x2A8D::0x0101::MY57503596::INSTR")
+    key1 = instools.create_inst(Keysight_34465A, "key1", "USB0::0x2A8D::0x0101::MY57503556::INSTR")
     key2 = instools.create_inst(Keysight_34465A, "key2", "USB0::0x2A8D::0x0101::MY57503135::INSTR")
 
-    from qcodes.instrument_drivers.rohde_schwarz.SGS100A import RohdeSchwarz_SGS100A
-    RF = instools.create_inst(RohdeSchwarz_SGS100A, 'RF', address="TCPIP0::169.254.2.20")
-    LO = instools.create_inst(RohdeSchwarz_SGS100A, 'LO', address="TCPIP0::169.254.234.107")
+    # from qcodes.instrument_drivers.rohde_schwarz.SGS100A import RohdeSchwarz_SGS100A
+    # RF = instools.create_inst(RohdeSchwarz_SGS100A, 'RF', address="TCPIP0::169.254.2.20")
+    # LO = instools.create_inst(RohdeSchwarz_SGS100A, 'LO', address="TCPIP0::169.254.234.107")
 
-    from qcodes.instrument_drivers.AlazarTech.ATS9360 import AlazarTech_ATS9360
-    alazar = instools.create_inst(AlazarTech_ATS9360, 'alazar')
+    # from qcodes.instrument_drivers.AlazarTech.ATS9360 import AlazarTech_ATS9360
+    # alazar = instools.create_inst(AlazarTech_ATS9360, 'alazar')
 
-    from qcodes.instrument_drivers.tektronix.AWG5014 import Tektronix_AWG5014
-    awg = instools.create_inst(Tektronix_AWG5014, 'awg', address="TCPIP0::169.254.183.196::inst0::INSTR")
+    # from qcodes.instrument_drivers.tektronix.AWG5014 import Tektronix_AWG5014
+    # awg = instools.create_inst(Tektronix_AWG5014, 'awg', address="TCPIP0::169.254.183.196::inst0::INSTR")
 
-    from qcodes.instrument_drivers.rigol.DG4000 import Rigol_DG4000
-    fg = instools.create_inst(Rigol_DG4000, 'fg', address="TCPIP0::169.254.190.44::inst0::INSTR")
+    # from qcodes.instrument_drivers.rigol.DG4000 import Rigol_DG4000
+    # fg = instools.create_inst(Rigol_DG4000, 'fg', address="TCPIP0::169.254.190.44::inst0::INSTR")
 
     for i in range(1,16):
         ivvi.parameters['dac{}'.format(i)].set_step(0.5)
         ivvi.parameters['dac{}'.format(i)].set_delay(0.001)
 
-    return qc.Station(ivvi, RF, LO, alazar, awg, fg, key1, key2)
+    return qc.Station(ivvi, key1, key2) # , RF, LO, alazar, awg, fg, key1, key2)
 
 
 ###  Setting some reasonable initial values
@@ -134,12 +134,12 @@ def set_instrument_defaults(station):
     ivvi.dac7.set_step(5)
 
     # RF Sources
-    station.RF.power(-55)
-    station.RF.frequency(538.2e6)
-    station.RF.on()
-    station.LO.power(22)
-    station.LO.frequency(station.RF.frequency()+20e6)
-    station.LO.on()
+    # station.RF.power(-55)
+    # station.RF.frequency(538.2e6)
+    # station.RF.on()
+    # station.LO.power(22)
+    # station.LO.frequency(station.RF.frequency()+20e6)
+    # station.LO.on()
 
 
 if __name__ == '__main__':
