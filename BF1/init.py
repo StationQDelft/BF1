104334b5ac3c1995feceb29f178a5ab7dff2bf7b
Wolfgang Pfaff <wolfgangpfff@gmai.com>
2018-05-24 13:11:35 +0200

    latest work; also BF1 is a proper package now.

---
+++ BF1/init.py
@@ -0,0 +1,2 @@
+import qcodes as qc
+qc.config['core']['db_location'] = r"D:\OneDrive\BF1\Data\experiments.db"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='BF1',
+    version='0.0.1',
+    description='BlueFors1 measurement repository',
+    author='Wolfgang Pfaff',
+    author_email='wolfgangpfff@gmail.com',
+    url='https://github.com/kouwenhovenlab',
+)
